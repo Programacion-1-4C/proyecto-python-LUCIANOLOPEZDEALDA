d8df37873ab3d1fa71046ee23d162d086e1fde45
LucianoLopezDeAlda <Lucianolopezdealda@gmail.com>
2022-08-10 14:13:10 -0300

    Empeze el proyecto e hice 4 funciones

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from funciones import menu
+from Funciones import director_tecnico
+from Funciones import jugador
 
-if __name__ == "__main__":
-    menu()
+lista_equipos = []
+jugador_fichado = []
+
+print('Hola bienvenido/a Futbol Manager!')
+print('Elige que quieres ser:')
+while True:
+    print(
+        '1. Director Tecnico\n'
+        '2. Jugador'
+        )
+    eleccion = int(input('>>> '))
+
+    if eleccion == 1:
+        director_tecnico()
+
+    elif eleccion == 2:
+        jugador()
